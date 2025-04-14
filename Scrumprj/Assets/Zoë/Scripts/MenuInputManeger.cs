1a14756d87fb1f0d0c9377b16d4700cedb2ee2b2
Zoe Ladino <zoeladino@hotmail.com>
2025-04-14 11:42:58 +0200

    outline highlight
    Er is nu een highlight die je om objecten heen kan doen

--- Scrumprj/Assets/Zoë/Scripts/MenuInputManeger.cs
+++ Scrumprj/Assets/Zoë/Scripts/MenuInputManeger.cs
@@ -3,5 +3,19 @@ using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 public class MenuInputManeger : MonoBehaviour
 {
- 
+
+    public void OnPlayPressed()
+    {
+
+    }
+
+    public void OnPausePressed()
+    {
+
+    }
+
+    public void OnQuitPressed()
+    {
+        Application.Quit();
+    }
 }
